12cfdf15641a30a67b2b1f3eb73918e94e80bb1b
Etienne Dreyer <etienne.dreyer@weizmann.ac.il>
2024-06-09 17:50:49 +0300

    Dmitrii changes

---
+++ PhysObjectExtractor/src/GenJetAnalyzer.cc
@@ -0,0 +1,239 @@
+// -*- C++ -*-
+//
+// Package:    GenJetAnalyzer
+// Class:      GenJetAnalyzer
+//
+
+// system include files
+#include <memory>
+#include <TMath.h>
+// user include files
+#include "FWCore/Framework/interface/Frameworkfwd.h"
+#include "FWCore/Framework/interface/EDAnalyzer.h"
+#include "FWCore/Framework/interface/Event.h"
+#include "FWCore/Framework/interface/MakerMacros.h"
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+#include "CommonTools/UtilAlgos/interface/TFileService.h"
+#include "DataFormats/Common/interface/Ref.h"
+#include "FWCore/Framework/interface/ESHandle.h"
+#include "FWCore/Framework/interface/EventSetup.h"
+#include "FWCore/MessageLogger/interface/MessageLogger.h"
+#include "FWCore/ServiceRegistry/interface/Service.h"
+#include "math.h"
+
+// classes to extract PFJet information
+#include "DataFormats/JetReco/interface/GenJet.h"
+#include "DataFormats/JetReco/interface/GenJetCollection.h"
+#include "DataFormats/VertexReco/interface/Vertex.h"
+#include "DataFormats/VertexReco/interface/VertexFwd.h"
+#include "DataFormats/JetReco/interface/Jet.h"
+#include "SimDataFormats/JetMatching/interface/JetFlavourInfo.h"
+#include "SimDataFormats/JetMatching/interface/JetFlavourInfoMatching.h"
+#include "DataFormats/JetReco/interface/CaloJet.h"
+#include "DataFormats/RecoCandidate/interface/RecoCandidate.h"
+
+// classes to save data
+#include "TTree.h"
+#include "TFile.h"
+#include <vector>
+
+#include "TRandom3.h"
+
+//
+// class declaration
+//
+
+class GenJetAnalyzer : public edm::EDAnalyzer
+{
+public:
+  explicit GenJetAnalyzer(const edm::ParameterSet &);
+  ~GenJetAnalyzer();
+
+  static void fillDescriptions(edm::ConfigurationDescriptions &descriptions);
+  std::vector<float> factorLookup(float eta);
+
+private:
+  virtual void beginJob();
+  virtual void analyze(const edm::Event &, const edm::EventSetup &);;
+  virtual void endJob();
+  virtual void beginRun(edm::Run const &, edm::EventSetup const &);
+  virtual void endRun(edm::Run const &, edm::EventSetup const &);
+  virtual void beginLuminosityBlock(edm::LuminosityBlock const &, edm::EventSetup const &);
+  virtual void endLuminosityBlock(edm::LuminosityBlock const &, edm::EventSetup const &) override;
+
+  // declare the input tag for PFJetCollection
+  edm::InputTag jetInput;
+
+  // ----------member data ---------------------------
+  
+  int numjet; // number of jets in the event
+  TTree *mtree;
+  std::vector<float> jet_e;
+  std::vector<float> jet_pt;
+  std::vector<float> jet_px;
+  std::vector<float> jet_py;
+  std::vector<float> jet_pz;
+  std::vector<float> jet_eta;
+  std::vector<float> jet_phi;
+  std::vector<float> jet_ch;
+  std::vector<float> jet_mass;
+};
+
+//
+// constants, enums and typedefs
+//
+
+//
+// static data member definitions
+//
+
+//
+// constructors and destructor
+//
+
+GenJetAnalyzer::GenJetAnalyzer(const edm::ParameterSet &iConfig)
+{
+  // now do what ever initialization is needed
+  jetInput = iConfig.getParameter<edm::InputTag>("InputCollection");
+  edm::Service<TFileService> fs;
+  mtree = fs->make<TTree>("Events", "Events");
+
+
+  mtree->Branch("numberjet", &numjet);
+  mtree->GetBranch("numberjet")->SetTitle("Number of Jets");
+  mtree->Branch("jet_e", &jet_e);
+  mtree->GetBranch("jet_e")->SetTitle("Uncorrected Jet Energy");
+  mtree->Branch("jet_pt", &jet_pt);
+  mtree->GetBranch("jet_pt")->SetTitle("Uncorrected Transverse Jet Momentum");
+  mtree->Branch("jet_px", &jet_px);
+  mtree->GetBranch("jet_px")->SetTitle("X-Component of Jet Momentum");
+  mtree->Branch("jet_py", &jet_py);
+  mtree->GetBranch("jet_py")->SetTitle("Y-Component of Jet Momentum");
+  mtree->Branch("jet_pz", &jet_pz);
+  mtree->GetBranch("jet_pz")->SetTitle("Z-Component of Jet Momentum");
+  mtree->Branch("jet_eta", &jet_eta);
+  mtree->GetBranch("jet_eta")->SetTitle("Jet Eta");
+  mtree->Branch("jet_phi", &jet_phi);
+  mtree->GetBranch("jet_phi")->SetTitle("Jet Phi");
+  mtree->Branch("jet_ch", &jet_ch);
+  mtree->GetBranch("jet_ch")->SetTitle("Jet Charge");
+  mtree->Branch("jet_mass", &jet_mass);
+  mtree->GetBranch("jet_mass")->SetTitle("Jet Mass");
+
+}
+
+GenJetAnalyzer::~GenJetAnalyzer()
+{
+  // do anything here that needs to be done at desctruction time
+  // (e.g. close files, deallocate resources etc.)
+}
+
+//
+// member functions
+//
+
+
+void GenJetAnalyzer::analyze(const edm::Event &iEvent, const edm::EventSetup &iSetup)
+{
+
+  using namespace edm;
+  using namespace std;
+
+  Handle<reco::GenJetCollection> myjets;
+  iEvent.getByLabel(jetInput, myjets);
+  Handle<reco::VertexCollection> vertices;
+  iEvent.getByLabel(InputTag("offlinePrimaryVertices"), vertices);
+
+  numjet = 0;
+  jet_e.clear();
+  jet_pt.clear();
+  jet_px.clear();
+  jet_py.clear();
+  jet_pz.clear();
+  jet_eta.clear();
+  jet_phi.clear();
+  jet_ch.clear();
+  jet_mass.clear();
+
+  if (myjets.isValid())
+  {
+
+    int min_pt = 20;
+
+    for (reco::GenJetCollection::const_iterator itjet = myjets->begin(); itjet != myjets->end(); ++itjet)
+    {
+      if (itjet->pt() > min_pt){
+        jet_e.push_back(itjet->energy());
+        jet_pt.push_back(itjet->pt());
+        jet_px.push_back(itjet->px());
+        jet_py.push_back(itjet->py());
+        jet_pz.push_back(itjet->pz());
+        jet_eta.push_back(itjet->eta());
+        jet_phi.push_back(itjet->phi());
+        jet_ch.push_back(itjet->charge());
+        jet_mass.push_back(itjet->mass());
+        ++numjet;
+      }
+    }
+  }
+
+  mtree->Fill();
+  return;
+}
+
+// ------------ method called once each job just before starting event loop  ------------
+void GenJetAnalyzer::beginJob()
+{
+}
+
+// ------------ method called once each job just after ending the event loop  ------------
+void GenJetAnalyzer::endJob()
+{
+}
+
+// ------------ method called when starting to processes a run  ------------
+void GenJetAnalyzer::beginRun(edm::Run const &, edm::EventSetup const &)
+{
+}
+
+// ------------ method called when ending the processing of a run  ------------
+void GenJetAnalyzer::endRun(edm::Run const &, edm::EventSetup const &)
+{
+}
+// ------------ method called when starting to processes a luminosity block  ------------
+void GenJetAnalyzer::beginLuminosityBlock(edm::LuminosityBlock const &, edm::EventSetup const &)
+{
+}
+
+// ------------ method called when ending the processing of a luminosity block  ------------
+void GenJetAnalyzer::endLuminosityBlock(edm::LuminosityBlock const &, edm::EventSetup const &)
+{
+}
+
+// ------------ method fills 'descriptions' with the allowed parameters for the module  ------------
+void GenJetAnalyzer::fillDescriptions(edm::ConfigurationDescriptions &descriptions)
+{
+  // The following says we do not know what parameters are allowed so do no validation
+  //  Please change this to state exactly what you do use, even if it is no parameters
+  edm::ParameterSetDescription desc;
+  desc.setUnknown();
+  descriptions.addDefault(desc);
+}
+std::vector<float>
+GenJetAnalyzer::factorLookup(float eta)
+{ // used in jet loop for JER factor value
+  // eta input is > 0
+  if (eta > 2.3)
+    return {1.288, 1.089, 1.488}; // {factor, factor_down, factor_up}
+  else if (eta > 1.7)
+    return {1.134, 1.042, 1.228};
+  else if (eta > 1.1)
+    return {1.096, 1.032, 1.161};
+  else if (eta > .5)
+    return {1.057, 1.001, 1.114};
+  else
+    return {1.052, 0.990, 1.115};
+}
+
+// define this as a plug-in
+DEFINE_FWK_MODULE(GenJetAnalyzer);
---
+++ PhysObjectExtractor/src/ParticleFlowAnalyzer.cc
@@ -0,0 +1,240 @@
+// -*- C++ -*-
+//
+// Package:    ParticleFlowAnalyzer
+// Class:      ParticleFlowAnalyzer
+//
+
+// system include files
+#include <memory>
+
+// user include files
+#include "FWCore/Framework/interface/Frameworkfwd.h"
+#include "FWCore/Framework/interface/EDAnalyzer.h"
+#include "FWCore/Framework/interface/Event.h"
+#include "FWCore/Framework/interface/MakerMacros.h"
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+#include "CommonTools/UtilAlgos/interface/TFileService.h"
+#include "FWCore/ServiceRegistry/interface/Service.h"
+
+//classes to extract ParticleFlowCandidate information
+#include "DataFormats/ParticleFlowCandidate/interface/PFCandidate.h"
+#include "DataFormats/ParticleFlowCandidate/interface/PFCandidateFwd.h"
+
+//classes to save data
+#include "TTree.h"
+#include "TFile.h"
+#include<vector>
+
+//
+// class declaration
+//
+
+class ParticleFlowAnalyzer : public edm::EDAnalyzer {
+   public:
+      explicit ParticleFlowAnalyzer(const edm::ParameterSet&);
+      ~ParticleFlowAnalyzer();
+
+      static void fillDescriptions(edm::ConfigurationDescriptions& descriptions);
+
+   private:
+      virtual void beginJob() ;
+      virtual void analyze(const edm::Event&, const edm::EventSetup&);
+      virtual void endJob() ;
+      virtual void beginRun(edm::Run const&, edm::EventSetup const&);
+      virtual void endRun(edm::Run const&, edm::EventSetup const&);
+      virtual void beginLuminosityBlock(edm::LuminosityBlock const&, edm::EventSetup const&);
+      virtual void endLuminosityBlock(edm::LuminosityBlock const&, edm::EventSetup const&) override;
+
+     std::vector<std::string>  particle;
+
+      // ----------member data ---------------------------
+
+      TTree *mtree;
+      
+      int numPFCand;
+      std::vector<float> PFCand_pt;
+      std::vector<float> PFCand_eta;
+      std::vector<float> PFCand_mass;
+      std::vector<int> PFCand_pdgId;
+      std::vector<float> PFCand_phi;
+      std::vector<float> PFCand_px;
+      std::vector<float> PFCand_py;
+      std::vector<float> PFCand_pz;
+
+      std::vector<float> PFCand_vx;
+      std::vector<float> PFCand_vy;
+      std::vector<float> PFCand_vz;
+};
+
+//
+// constants, enums and typedefs
+//
+
+//
+// static data member definitions
+//
+
+//
+// constructors and destructor
+//
+
+ParticleFlowAnalyzer::ParticleFlowAnalyzer(const edm::ParameterSet& iConfig):
+particle(iConfig.getParameter<std::vector<std::string> >("input_particle"))
+
+{
+//now do what ever initialization is needed
+	edm::Service<TFileService> fs;
+	mtree = fs->make<TTree>("Events", "Events");
+    
+    mtree->Branch("numPFCand",&numPFCand);
+    mtree->GetBranch("numPFCand")->SetTitle("number of pfc particles");
+    mtree->Branch("PFCand_pt",&PFCand_pt);
+    mtree->GetBranch("PFCand_pt")->SetTitle("pflow candidate transverse momentum");
+    mtree->Branch("PFCand_eta",&PFCand_eta);
+    mtree->GetBranch("PFCand_eta")->SetTitle("pflow candidate pseudorapidity");
+    mtree->Branch("PFCand_mass",&PFCand_mass);
+    mtree->GetBranch("PFCand_mass")->SetTitle("pflow candidate mass");
+    mtree->Branch("PFCand_pdgId",&PFCand_pdgId);
+    mtree->GetBranch("PFCand_pdgId")->SetTitle("pflow candidate PDG id");
+    mtree->Branch("PFCand_phi",&PFCand_phi);
+    mtree->GetBranch("PFCand_phi")->SetTitle("pflow candidate azimuthal angle of momentum vector");
+    mtree->Branch("PFCand_px",&PFCand_px);
+    mtree->GetBranch("PFCand_px")->SetTitle("pflow candidate x coordinate of momentum vector");
+    mtree->Branch("PFCand_py",&PFCand_py);
+    mtree->GetBranch("PFCand_py")->SetTitle("pflow candidate y coordinate of momentum vector");
+    mtree->Branch("PFCand_pz",&PFCand_pz);
+    mtree->GetBranch("PFCand_pz")->SetTitle("pflow candidate z coordinate of momentum vector");
+
+    mtree->Branch("PFCand_vx",&PFCand_vx);
+    mtree->GetBranch("PFCand_vx")->SetTitle("pflow candidate x coordinate of its vertex");
+    mtree->Branch("PFCand_vy",&PFCand_vy);
+    mtree->GetBranch("PFCand_vy")->SetTitle("pflow candidate y coordinate of its vertex");
+    mtree->Branch("PFCand_vz",&PFCand_vz);
+    mtree->GetBranch("PFCand_vz")->SetTitle("pflow candidate z coordinate of its vertex");
+
+}
+
+ParticleFlowAnalyzer::~ParticleFlowAnalyzer()
+{
+   // do anything here that needs to be done at desctruction time
+   // (e.g. close files, deallocate resources etc.)
+}
+
+
+//
+// member functions
+//
+
+// ------------ method called for each event  ------------
+void
+ParticleFlowAnalyzer::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup)
+{
+   using namespace edm;
+   using namespace std;
+   
+   numPFCand=0;
+   PFCand_pt.clear();
+   PFCand_eta.clear();
+   PFCand_mass.clear();
+   PFCand_pdgId.clear();
+   PFCand_phi.clear();
+   PFCand_px.clear();
+   PFCand_py.clear();
+   PFCand_pz.clear();
+
+   PFCand_vx.clear();
+   PFCand_vy.clear();
+   PFCand_vz.clear();
+
+   Handle<reco::PFCandidateCollection> pfcs;
+   iEvent.getByLabel("particleFlow", pfcs);
+   
+   unsigned int i;
+   // string s1,s2;
+   // std::vector<int> status_parsed;
+   // std::vector<int> pdgId_parsed;
+   // std::string delimiter = ":";
+
+   // for(i=0;i<particle.size();i++)
+   // {
+   //     //get status and pgdId from configuration
+   //     s1=particle[i].substr(0,particle[i].find(delimiter));
+   //     s2=particle[i].substr(particle[i].find(delimiter)+1,particle[i].size());
+   //     //parse string to int
+   //     status_parsed.push_back(stoi(s1));
+   //     pdgId_parsed.push_back(stoi(s2));
+   // }
+  
+  if(pfcs.isValid())
+  {
+      numPFCand=pfcs->size();
+      for (reco::PFCandidateCollection::const_iterator itPFCand=pfcs->begin(); itPFCand!=pfcs->end(); ++itPFCand)
+      {
+         //loop trough all particles selected in configuration
+         for(i = 0; i < particle.size(); i++)
+         {
+         //    if((status_parsed[i]==itPFCand->status() && pdgId_parsed[i]==itPFCand->pdgId())||(status_parsed[i]==0 && pdgId_parsed[i]==0))
+         //  {
+            PFCand_pt.push_back(itPFCand->pt());
+            PFCand_eta.push_back(itPFCand->eta());
+            PFCand_mass.push_back(itPFCand->mass());
+            PFCand_pdgId.push_back(itPFCand->pdgId());
+            PFCand_phi.push_back(itPFCand->phi());
+            PFCand_px.push_back(itPFCand->px());
+            PFCand_py.push_back(itPFCand->py());
+            PFCand_pz.push_back(itPFCand->pz());
+
+            PFCand_vx.push_back(itPFCand->vx());
+            PFCand_vy.push_back(itPFCand->vy());
+            PFCand_vz.push_back(itPFCand->vz());
+         //  }
+         }               
+      }
+  }
+	
+  mtree->Fill();
+  return;
+
+}
+
+// ------------ method called once each job just before starting event loop  ------------
+void
+ParticleFlowAnalyzer::beginJob()
+{}
+
+// ------------ method called once each job just after ending the event loop  ------------
+void
+ParticleFlowAnalyzer::endJob()
+{}
+
+// ------------ method called when starting to processes a run  ------------
+void
+ParticleFlowAnalyzer::beginRun(edm::Run const&, edm::EventSetup const&)
+{}
+
+// ------------ method called when ending the processing of a run  ------------
+void
+ParticleFlowAnalyzer::endRun(edm::Run const&, edm::EventSetup const&)
+{}
+// ------------ method called when starting to processes a luminosity block  ------------
+void
+ParticleFlowAnalyzer::beginLuminosityBlock(edm::LuminosityBlock const&, edm::EventSetup const&)
+{}
+
+// ------------ method called when ending the processing of a luminosity block  ------------
+void
+ParticleFlowAnalyzer::endLuminosityBlock(edm::LuminosityBlock const&, edm::EventSetup const&)
+{}
+
+// ------------ method fills 'descriptions' with the allowed parameters for the module  ------------
+void
+ParticleFlowAnalyzer::fillDescriptions(edm::ConfigurationDescriptions& descriptions) {
+ //The following says we do not know what parameters are allowed so do no validation
+  // Please change this to state exactly what you do use, even if it is no parameters
+  edm::ParameterSetDescription desc;
+  desc.setUnknown();
+  descriptions.addDefault(desc);
+}
+
+//define this as a plug-in
+DEFINE_FWK_MODULE(ParticleFlowAnalyzer);
