47834713d7a009b4fa8b1987686c1fa12220eb1d
Etienne Dreyer <etienne.dreyer@weizmann.ac.il>
2024-06-10 12:02:29 +0300

    more info in setup.sh

--- setup.sh
+++ setup.sh
@@ -1,11 +1,14 @@
+cd ../../../
+
 conda activate appenv
 
 source /cvmfs/cms.cern.ch/cmsset_default.sh
 
 cmssw-el5
 cmsrel CMSSW_5_3_11_patch6
-cd CMSSW_5_3_11_patch6
 
-cd src
+cd CMSSW_5_3_11_patch6/src
 cmsenv
-cd ../../
+
+cd PhysObjectExtractorTool/PhysObjectExtractor/
+scram b
